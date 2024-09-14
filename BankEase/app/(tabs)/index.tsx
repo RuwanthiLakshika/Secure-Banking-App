b2961bd8683ea060204fbb04dc57b20488254fb8
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-09-14 10:04:58 +0530

    initial changes

---
+++ BankEase/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+
+const Index = () => {
+  return (
+    <View>
+      <Text style={tw`text-2xl font-bold text-red-500`}>Welcome to BankEase</Text>
+    </View>
+  )
+}
+
+export default Index
